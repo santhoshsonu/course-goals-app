2878fefaf7ea18d92f0a1cc1505d76261447e9ae
santhoshsonu <santhosheshwaramma@gmail.com>
2022-05-15 20:55:36 +0530

    Validate and dynamic style course input

--- src/Components/CourseGoals/CourseInput/CourseInput.js
+++ src/Components/CourseGoals/CourseInput/CourseInput.js
@@ -7,21 +7,31 @@ import './CourseInput.css';
 
 const CourseInput = props => {
     const [goalValue, setGoalValue] = useState('');
+    const [isValidGoal, setIsValidGoal] = useState(true);
 
     const goalInputChangeHandler = (event) => {
-        setGoalValue(event.target.value);
+        const value = event.target.value;
+        setIsValidGoal(value.trim().length > 0);
+        setGoalValue(value);
     };
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
+        if (goalValue.trim().length === 0) {
+            if (isValidGoal) { setIsValidGoal(false); }
+            return;
+        }
         props.onAddGoal(goalValue);
     };
 
     return (
         <form onSubmit={formSubmitHandler}>
             <div className="form-control">
-                <label>Course Goal</label>
-                <input type="text" onChange={goalInputChangeHandler} />
+                <label style={{ color: isValidGoal ? 'black' : 'red' }}>Course Goal</label>
+                <input style={{
+                    borderColor: isValidGoal ? 'black' : 'red',
+                    background: isValidGoal ? 'transparent' : 'salmon'
+                }} type="text" onChange={goalInputChangeHandler} />
             </div>
             <Button type="submit">Add Goal</Button>
         </form>
